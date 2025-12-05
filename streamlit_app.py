f9d6f1bc599d3c85b2879ec1e1cee32483775a06
vinayh4 <vinay.h4@gmail.com>
2025-12-05 21:58:35 +0530

    update search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ st.write(
 
 cnx = st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')) 
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')) 
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     "Chose up to 5 ingredients:",
